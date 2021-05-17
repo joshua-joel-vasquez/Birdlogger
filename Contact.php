d7d7ad7fd281a8b4b05f3af9d3545c2f5cadbef9
Joshua Vasquez <50846418+joshua-joel-vasquez@users.noreply.github.com>
2021-05-16 22:59:25 -0500

    Add files via upload

--- Contact.php
+++ Contact.php
@@ -11,14 +11,13 @@ session_start();
 
 <body>
 <div id="container">
-		<div id="mainpic">
-        	<h1>BirdLogger</h1>
+        <div id="mainpic">
+            <h1>BirdLogger</h1>
             <h2>The best bird database</h2>
-            <p>&nbsp;</p>
             <?php  if (isset($_SESSION["loggedin"])) : ?>
                 <h3> <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b></h3>
             <?php endif ?>
-        </div>   
+        </div>
         
         <div id="menu">
             <ul>
@@ -36,27 +35,17 @@ session_start();
             </ul>
         </div>
         
-		<div id="content">
-        	<h2>You may use this template in any manner you like. All I ask is that you leave the link back to my site at the bottom of the page. </h2>
-        	<p>&nbsp;</p>
-           	<p>&nbsp;</p>
-       	  <h3>Template Notes</h3>
-        	<p>The main image can be changed by either replacing the current image with another one of the same size (900x402), or using a new one of what ever dimensions you'd like.  If you choose the latter, you must open up style.css and change the dimensions of #mainpic, as well as the file name if that is different. If you would like to move the heading around in the above image, find &quot;#mainpic h1&quot; in style.css and modify it's &quot;left&quot; and &quot;top&quot; properties, this is also true for the h2 tag.</p>
-        	<p>&nbsp;</p>
-<h3>More information</h3>
-        	<p>I decided to leave the content portion open for the templates users to do as they wish with a blank canvas. I don't like to restrict my users too much, and for this reason I leave the defining of any content related styles to you.</p>
-        	<p>&nbsp;</p>
-        	<h3>Template Notes</h3>
-            <p>The main image can be changed by either replacing the current image with another one of the same size (900x402), or using a new one of what ever dimensions you'd like.  If you choose the latter, you must open up style.css and change the dimensions of #mainpic, as well as the file name if that is different. If you would like to move the heading around in the above image, find &quot;#mainpic h1&quot; in style.css and modify it's &quot;left&quot; and &quot;top&quot; properties, this is also true for the h2 tag.</p>
+        <div id="content">
+            <h2>Please reach out to us with any questions or comments about our website</h2>
+            <p>...just dont expect us to reply</p>
             <p>&nbsp;</p>
-        	<h3>More information</h3>
-            <p>I decided to leave the content portion open for the templates users to do as they wish with a blank canvas. I don't like to restrict my users too much, and for this reason I leave the defining of any content related styles to you.</p>
             <p>&nbsp;</p>
-        	<p>&nbsp;</p>
-        	<p>&nbsp;</p>
+          <h3>Green Kingfisher Inc.</h3>
+            <p>Visit our project webpage at <a href="https://green-kingfishers.github.io/">green-kingfishers.github.io</a> to view our work and keep up with our progress.</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
             <p>&nbsp;</p>
             
-            <div id="footer"><h3>Bird database</div>
       </div>
    </div>
 </body>
